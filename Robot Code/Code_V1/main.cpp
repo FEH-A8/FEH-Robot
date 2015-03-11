1732f99119783792cc9303edf38e847af91ca631
Sway623 <abuswaysamer@yahoo.com>
2015-03-11 09:34:17 -0400

    Update main.cpp

--- Robot Code/Code_V1/main.cpp
+++ Robot Code/Code_V1/main.cpp
@@ -1,23 +1,424 @@
 #include <FEHLCD.h>
 #include <FEHIO.h>
 #include <FEHUtility.h>
+#include <FEHMotor.h>
+#include <FEHServo.h>
+#include <FEHRPS.h>
 
+//Declarations for encoders & motors
+ButtonBoard buttons(FEHIO::Bank3);
+FEHEncoder right_encoder(FEHIO::P1_0);
+FEHEncoder left_encoder(FEHIO::P2_0);
+FEHMotor right_motor(FEHMotor::Motor0);
+FEHMotor left_motor(FEHMotor::Motor1);
+FEHServo servo(FEHServo::Servo7);
+FEHServo servoSalt(FEHServo::Servo4);
+AnalogInputPin CdS(FEHIO::P0_0);
+
+//declares RPS location constants for key locations
+const float START_LIGHT_X = 18;
+const float START_LIGHT_Y = 30;
+const float CRANK_X = 28.8;
+const float CRANK_Y = 58.3;
+const float SALT_X = 27.9;
+const float SALT_Y = 8.4;
+const float BUTTONS_X_RB = 15.099;
+const float BUTTONS_Y = 64.099;
+const float BUTTONS_X_W = 13.8;
+const float BUTTONS_X_W = 62.8;
+const float GARAGE_X = 6.4;
+const float GARAGE_Y = 59.099;
+const float SWITCH_X = 13.669;
+const float SWITCH_Y = 9.9;
+
+const int percent = 60; //sets the motor percent for the rest of the code
+const int toSlow = 25; //this int will be the fix required for the robot to travel among the course
+const float cts_per_in= 3.704; //counts per inch
+const float cts_per_deg = .1776; //counts per degree
+
+//declares prototypes for functions
+void goToCrank();
+void goToButtons();
+void goToSalt();
+void goToGarage();
+void goToSwitch();
+
+void turnCrank();
+void pushButtons();
+void getSalt();
+void depositSalt();
+void toggleSwitch();
+
+void check_heading(float heading);
+
+void move(int percent, int counts);
+void turn_left(int percent, int counts);
+void turn_right(int percent, int counts);
+
+/*
+ *This method allows the robot to move for a certain number of counts
+ * @param counts
+ *              the number of counts for which the robot should move forward
+ * @param percent
+ *              the motor power percentage at which the robot will travel.
+ * @convention
+ *              if @param percent is negative, robot will move backwards
+ *              else robot will move forwards
+ */
+void move(int percent, int counts) //using encoders
+{
+    //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+
+    //Set both motors to desired percent
+    right_motor.SetPercent(percent);
+    left_motor.SetPercent(percent);
+
+    //While the average of the left and right encoder are less than counts,
+    //keep running motors
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+
+    //Turn off motors
+    right_motor.Stop();
+    left_motor.Stop();
+} //move
+
+/*
+ *This method allows the robot to turn right for a certain number of counts
+ * @param counts
+ *              the number of counts for which the robot should be turning
+ * @param percent
+ *              the percent at which the motor will be powered during turning
+ */
+void turn_right(int percent, int counts) //using encoders
+{
+    //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+
+    //Set both motors to desired percent
+    //hint: set right motor forward, left motor backward
+
+    right_motor.SetPercent(-percent);
+    left_motor.SetPercent(percent);
+
+
+    //While the average of the left and right encoder are less than counts,
+    //keep running motors
+
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+
+    //Turn off motors
+    right_motor.Stop();
+    left_motor.Stop();
+} //turn_right
+
+/*
+ *This method allows the robot to turn left for a certain number of counts
+ * @param counts
+ *              the number of counts for which the robot should be turning
+ * @param percent
+ *              the percent at which the motor will be powered during turning
+ */
+void turn_left(int percent, int counts) //using encoders
+{
+    //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+
+    //Set both motors to desired percent
+
+    right_motor.SetPercent(percent);
+    left_motor.SetPercent(-percent);
+
+    //While the average of the left and right encoder are less than counts,
+    //keep running motors
+
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+
+    //Turn off motors
+    right_motor.Stop();
+    left_motor.Stop();
+} //turn_left
+
+/*
+ *This method will allow the robot to push the buttons on the side of the BOO in order
+ */
+void pushButtons(){
+    int counts = cts_per_in * 1;
+    if (RPS.RedButtonOrder() == 1){
+        servo.SetDegree(25); //prepare to hit red button
+        Sleep(100);
+        move(percent-toSlow, counts); //drive forward and push button
+        Sleep(100);
+        move(-(percent-toSlow), counts); //back up
+        if (RPS.WhiteButtonOrder() == 2){
+            servo.SetDegree(90); //prepare to hit white button
+            Sleep(100);
+            move(percent-toSlow, counts); //drive forward and push button
+            Sleep(100);
+            move(-(percent-toSlow), counts); //back up
+            Sleep(100);
+            servo.SetDegree(150); //prepare to hit blue button
+            Sleep(100);
+            move(percent-toSlow, counts); //drive forward and push button
+            Sleep(100);
+            move(-(percent-toSlow), counts); //back up
+        } else{ //if blue is second
+            servo.SetDegree(150); //prepare to hit blue button
+            Sleep(100);
+            move(percent-toSlow, counts); //drive forward and push button
+            Sleep(100);
+            move(-(percent-toSlow), counts); //back up
+            Sleep(100);
+            servo.SetDegree(90); //prepare to hit white button
+            Sleep(100);
+            move(percent-toSlow, counts); //drive forward and push button
+            Sleep(100);
+            move(-(percent-toSlow), counts); //back up
+        } //else
+    } else if (RPS.WhiteButtonOrder() == 1){
+        servo.SetDegree(90); //prepare to hit white button
+        Sleep(100);
+        move(percent-toSlow, counts); //drive forward and push button
+        Sleep(100);
+        move(-(percent-toSlow), counts); //back up
+        if (RPS.RedButtonOrder() == 2){
+            servo.SetDegree(25); //prepare to hit red button
+            Sleep(100);
+            move(percent-toSlow, counts); //drive forward and push button
+            Sleep(100);
+            move(-(percent-toSlow), counts); //back up
+            Sleep(100);
+            servo.SetDegree(150); //prepare to hit blue button
+            Sleep(100);
+            move(percent-toSlow, counts); //drive forward and push button
+            Sleep(100);
+            move(-(percent-toSlow), counts); //back up
+        } else{ //if blue is second
+            servo.SetDegree(150); //prepare to hit blue button
+            Sleep(100);
+            move(percent-toSlow, counts); //drive forward and push button
+            Sleep(100);
+            move(-(percent-toSlow), counts); //back up
+            Sleep(100);
+            servo.SetDegree(25); //prepare to hit red button
+            Sleep(100);
+            move(percent-toSlow, counts); //drive forward and push button
+            Sleep(100);
+            move(-(percent-toSlow), counts); //back up
+        } //else
+    } else if (RPS.BlueButtonOrder() == 1){
+        servo.SetDegree(150); //prepare to hit blue button
+        Sleep(100);
+        move(percent-toSlow, counts); //drive forward and push button
+        Sleep(100);
+        move(-(percent-toSlow), counts); //back up
+        if (RPS.RedButtonOrder() == 2){
+            servo.SetDegree(25);
+            Sleep(100);
+            move(percent-toSlow, counts); //drive forward and push button
+            Sleep(100);
+            move(-(percent-toSlow), counts); //back up
+            Sleep(100);
+            servo.SetDegree(90); //prepare to hit white button
+            Sleep(100);
+            move(percent-toSlow, counts); //drive forward and push button
+            Sleep(100);
+            move(-(percent-toSlow), counts); //back up
+        } else{ //if white is second
+            servo.SetDegree(90); //prepare to hit white button
+            Sleep(100);
+            move(percent-toSlow, counts); //drive forward and push button
+            Sleep(100);
+            move(-(percent-toSlow), counts); //back up
+            Sleep(100);
+            servo.SetDegree(25); //prepare to hit red button
+            Sleep(100);
+            move(percent-toSlow, counts); //drive forward and push button
+            Sleep(100);
+            move(-(percent-toSlow), counts); //back up
+        } //else
+    }
+} //pushButtons
+
+/*
+ * This method will pick up the salt bag.
+ * The salt bag is to be dragged by the robot for the remainder of the course,
+ * until it is deposited in the garage.
+ */
+void getSalt(){
+    servoSalt.SetDegree(90);
+} //getSalt
+
+/*
+ * This method will deposit the salt into the garage.
+ */
+void depositSalt(){
+    servoSalt.SetDegree(0);
+    move(-(percent-toSlow), counts); //back up and push salt into garage
+} //depositSalt
+
+/*
+ * This method will toggle the oil switch.
+ * @convention
+ *          if RPS.OilDirec() is 0, then switch must be pushed to the left
+ *          else the switch must be pushed to the right.
+ */
+void toggleSwitch(){
+    if (RPS.OilDirec()==0){
+        turn_right(percent-toSlow, cts_per_deg*45);
+        servoSalt.SetDegree(45);
+        turn_left(percent-toSlow, cts_per_deg*50);
+    } else{
+        turn_left(percent-toSlow, cts_per_deg*45);
+        servoSalt.SetDegree(45);
+        turn_right(percent-toSlow, cts_per_deg*50);
+    }
+} //toggleSwitch
+
+/*
+ * This method will take the robot from the start light, to the salt bag.
+ */
+void goToSalt(){
+
+} //goToSalt
+
+/*
+ * This method will take the robot from the salt bag to the crank.
+ */
+void goToCrank(){
+
+} //goToCrank
+
+/*
+ * This method will take the robot from the crank to the buttons
+ */
+void goToButtons(){
+
+} //goToButtons
+
+/*
+ * This method will take the robot from the buttons, to the garage.
+ * After this method is called, the robot should be in good position
+ * to deposit the salt into the garage.
+ */
+void goToGarage(){
+
+} //goToGarage
+
+/*
+ * Finally, this method will take the robot from the garage, to the oil switch.
+ * After this method is called, the robot should go down the avalanche ramp and
+ * be placed in a good position to toggle the switch.
+ */
+void goToSwitch(){
+
+} //goToSwitch
+
+/*
+ * This method will check the current heading of the robot, and set the heading of the robot to @param heading
+ * @param heading
+ *          the heading at which the robot wants to be
+ */
+void check_heading(float heading){
+    const int turnPercent = 50;
+    float change = (int)abs(heading-RPS.Heading());
+    if(change>180){ change = 360-change; }
+    while(change > 2){
+        float curr_Heading = RPS.Heading();
+        if(curr_Heading < heading || (curr_Heading > 270 && heading < 90)){ //Turn right
+            right_motor.SetPercent(-turnPercent);
+            left_motor.SetPercent(turnPercent);
+        } //if
+        else { //Turn left
+            right_motor.SetPercent(turnPercent);
+            left_motor.SetPercent(-turnPercent);
+        } //else
+        Sleep(30);
+        change = (int)abs(heading-RPS.Heading());
+        if(change>180){
+            change = 360-change;
+        } //if
+    } //while
+    right_motor.SetPercent(0);
+    left_motor.SetPercent(0);
+} //check_heading
+
+/*
+ * This method was created to efficiently complete performance test 4
+ */
+void performanceTest4(){
+    turn_left(percent-toSlow, cts_per_deg*55); //angle robot towards salt
+    move(-percent, cts_per_in*27.5); //move to the salt
+    //check x and y
+    //check heading
+    getSalt();
+    turn_right(percent-toSlow, cts_per_deg*55); //prepare to go up ramp
+    //check heading
+    move(percent, cts_per_in*46); //go up ramp
+    turn_right(percent - toSlow, cts_per_deg*90); //turn towards garage
+    //check x and y
+    //check heading
+    move(-percent, cts_per_in*27); //get to garage
+    depositSalt();
+    move(percent, cts_per_in*27); //get back to last point
+    turn_left(percent, cts_per_deg*90); //prepare to go down ramp
+    move(percent, cts_per_in*27); //go down ramp
+    turn_left(percent-toSlow, cts_per_deg*45); //prepare to go to switch
+    move(percent, cts_per_in*27); //get to switch
+    //check x and y
+    //check heading
+    toggleSwitch();
+}//performanceTest4
+
+/*
+ * The main method
+ */
 int main(void)
 {
-    ButtonBoard buttons( FEHIO::Bank3 );
-
     LCD.Clear( FEHLCD::Black );
     LCD.SetFontColor( FEHLCD::White );
 
-    while( true )
-    {
-        if( buttons.MiddlePressed() )
-        {
-            LCD.WriteLine( "Hello World!" );
-            Sleep( 100 );
-            LCD.WriteLine("Hello");
-        }
-    }
-    return 0;
-}
+    const arrayLength = 5; //sets length of task array
 
+    /*
+     * Initialize task array.
+     * Elements of which are to be used in switch case in main.
+     */
+    int taskArray[arrayLength] = {0, 1, 2, 3, 4};
+
+    //initialize positions of servo motors
+    servoSalt.SetDegree(0);
+    servo.SetDegree(0);
+
+    while(CdS.Value()>1); //start on the light
+
+    performanceTest4();
+
+//    for (int i=0; i<arrayLength; i++){
+//        switch (taskArray[i]){
+//        case 0:
+//            goToSalt();
+//            getSalt();
+//            break;
+//        case 1:
+//            goToCrank();
+//            turnCrank();
+//            break;
+//        case 2:
+//            goToButtons();
+//            turnButtons();
+//            break;
+//        case 3:
+//            goToGarage();
+//            depositSalt();
+//        case 4:
+//            goToSwitch();
+//            toggleSwitch();
+//        } //switch
+//    } //for
+
+    return 0;
+} //main
